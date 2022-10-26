bf22e8e98f7f345a7528081e6bc96151951e432c
Moamer <58955632+Moamer87@users.noreply.github.com>
2022-10-26 20:22:41 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const input = "turpentine and turtles";
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+const result = [];
+
+for (let index = 0; index < input.length; index++) {
+    //console.log('index is ' + i);
+}
+
+    if (input[index] === 'e') {
+        resultArray.push(input[index]);
+    }
+
+    if (input[index] === 'u') {
+        resultArray.push(input[index]);
+    }
+
+for (let v = 0; v < vowels.length; v++) {
+    //console.log('v is' + v);
+}
+
+    if (input[i] === vowels[v]) {
+        resultArray.push(input[index]);
+        //console.log(input[i]);
+    }
+
+console.log(resultArray);
+
+const resultString = resultArray.join('').toUpperCase();
+
+console.log(resultString);
